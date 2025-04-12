2184b532f6dcffad64adf73cc1bdd3e9400fb53c
Sebastian Tremblay <tremblay.se@northeastern.edu>
2025-04-12 15:45:40 -0400

    Preprocessing refactor for extracting numpy arrays

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,34 @@
-from   scipy.io   import  loadmat
-import numpy      as      np
-import matplotlib.pylab as plt
+import numpy as np
 import os
 from PIL import Image
 
 
-def image_generator(folder, target_size=None):
+def image_generator(folder: str, target_size: tuple[int, int] = None) -> list[np.ndarray]:
     images = []
+    print(f"\nProcessing folder: {folder}")
 
     for filename in os.listdir(folder):
         if filename.lower().endswith('.jpg'):
             img_path = os.path.join(folder, filename)
             with Image.open(img_path) as img:
+                # Resize image if target_size is provided
                 if target_size:
                     img = img.resize(target_size)
-                # Convert image to a NumPy array (optionally normalize, etc.)
+
+                # Convert image to a NumPy array
                 img_array = np.array(img)
                 images.append(img_array)
 
     return images
 
-images = image_generator('./classifiedData/frontfacingImages')
-images2 = image_generator('./classifiedData/facingAwayImages')
 
-print(len(images))
-print(len(images2))
+# Load images
+target_size = (224, 224)  # Standard size used in many CNN architectures
+positive_images = image_generator(
+    './classifiedData/frontfacingImages', target_size)
+negative_images = image_generator(
+    './classifiedData/facingAwayImages', target_size)
 
-# plt.imshow(images[1000])            # For grayscale images, add cmap='gray'
-# plt.axis('off')                    # Optional: turn off the axis labels
-# plt.title("My Image")              # Optional: add a title to the plot
-# plt.show()                         # Display the image
+# Save images as numpy arrays
+np.save('./classifiedData/frontfacingImages.npy', positive_images)
+np.save('./classifiedData/facingAwayImages.npy', negative_images)
